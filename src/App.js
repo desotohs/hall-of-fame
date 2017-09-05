c4ef8c5c009183f994194c573e9324c039aea233
Zach Deibert <zachdeibert@gmail.com>
2017-09-05 09:55:29 -0500

    Added material css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import CDNotification from "./CDNotification";
+import "materialize-css/dist/css/materialize.css";
 
 export default class App extends React.Component {
     render() {
