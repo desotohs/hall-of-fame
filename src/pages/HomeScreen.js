c078318a22918d9deb6b6a762204e5d12af50717
Zach Deibert <zachdeibert@gmail.com>
2017-09-05 21:07:02 -0500

    Added some better styling to the home screen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,18 +2,34 @@ import React from "react";
 import video from "../media/video/fullscreenvideo.mp4";
 import "./HomeScreen.css";
 
-
-export default class HomeScreen extends React.Component {
+class Video extends React.Component {
     componentDidMount() {
         this.video.play();
     }
 
     render() {
         return (
+            <video loop ref={el => this.video = el} className={this.props.className}>
+                <source src={video} type="video/webm"/>
+            </video>
+        );
+    }
+}
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
             <div className="homescreen">
-                <video loop ref={el => this.video = el}>
-                    <source src={video} type="video/webm"/>
-                </video>
+                <Video className="blur" />
+                <Video />
+                <div className="controls">
+                    <div className="control">
+                        Hall of Fame
+                    </div>
+                    <div className="control">
+                        State Champions
+                    </div>
+                </div>
             </div>
         );
     }
