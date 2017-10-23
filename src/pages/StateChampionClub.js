8eb88d7236a4950f2459798f91c34244c7174fb6
Cody Moose <rcmoose2000@gmail.com>
2017-10-23 18:01:34 -0500

    Changed activity title to activity logo

--- src/pages/StateChampionClub.js
+++ src/pages/StateChampionClub.js
@@ -1,29 +1,52 @@
 import React from "react";
 import BackgroundVideo from "../components/BackgroundVideo";
-import { Link } from 'react-router-dom'
-import icon from "../media/images/logo.png";
-import bandIcon from "../media/images/Band.png";
-import boysBasketballIcon from "../media/images/Basketball.Boys.png";
-import girlsBasketballIcon from "../media/images/Basketball.Girls.png";
-import bowlingIcon from "../media/images/Bowling.png";
-import boysGolfIcon from "../media/images/Boys.Golf.png";
-import cheerIcon from "../media/images/Cheer.png";
-import debateIcon from "../media/images/Debate.png";
-import girlsSwimIcon from "../media/images/Girls Swim.png";
-import scholarsBowlIcon from "../media/images/Scholars Bowl.png";
-import soccerIcon from "../media/images/Soccer.png";
-import tennisIcon from "../media/images/Tennis.png";
-import theatreIcon from "../media/images/Theatre.png";
-import volleyballIcon from "../media/images/Volleyball.png";
-import "./StateChampions.css";
+import band from "../media/images/Band.png";
+import boysbasketball from "../media/images/Basketball.Boys.png";
+import girlsbasketball from "../media/images/Basketball.Girls.png";
+import bowling from "../media/images/Bowling.png";
+import boysgolf from "../media/images/Boys.Golf.png";
+import cheer from "../media/images/Cheer.png";
+import debate from "../media/images/Debate.png";
+import girlsswim from "../media/images/Girls Swim.png";
+import scholarsbowl from "../media/images/Scholars Bowl.png";
+import soccer from "../media/images/Soccer.png";
+import tennis from "../media/images/Tennis.png";
+import theatre from "../media/images/Theatre.png";
+import volleyball from "../media/images/Volleyball.png";
+import "./StateChampionClub.css";
 
 export default class StateChampions extends React.Component {
     render() {
         return (
-            <div className="statechampions">
-            <h2>{this.props.match.params.club.charAt(0).toUpperCase() + this.props.match.params.club.slice(1)}</h2>
-            <BackgroundVideo />
+            <div className="statechampionclub">
+                {/* <h2 className="clubtitle"> {
+                    doTitle(this.props.match.params.club)
+                } </h2> */}
+                <BackgroundVideo />
+                <img className="iconImage" src={images[doImageName(this.props.match.params.club)]} alt={doImageName(this.props.match.params.club)} />
             </div>
         );
     }
+     
+}
+var images = {"band": band, "boysbasketball": boysbasketball, "girlsbasketball": girlsbasketball,
+    "bowling": bowling, "boysgolf": boysgolf, "cheer": cheer, "debate": debate,"girlsswim": girlsswim,
+    "scholarsbowl": scholarsbowl, "soccer": soccer, "tennis": tennis, "theatre": theatre, "volleyball": volleyball};
+
+function doTitle(club){
+    var words = club.split("-")
+    var result = "";
+    for(var i = 0; i < words.length; i++){
+        result = result + words[i].charAt(0).toUpperCase() + words[i].slice(1) + " ";
+    }
+    return result
+}
+
+function doImageName(club){
+    var words = club.split("-")
+    var result = "";
+    for(var i = 0; i < words.length; i++){
+        result = result + words[i];
+    }
+    return result
 }
