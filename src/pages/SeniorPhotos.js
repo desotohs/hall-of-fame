fb6c72750dbf3236f69936c9e29ee4a9b53549a2
Cody Moose <rcmoose2000@gmail.com>
2017-10-27 09:30:20 -0500

    Text to green and borders gone on Senior Photos and State Champions pages

--- src/pages/SeniorPhotos.js
+++ src/pages/SeniorPhotos.js
@@ -8,64 +8,65 @@ export default class StateChampions extends React.Component {
         return (
             <div className="seniorphotos">
                 <BackgroundVideo />
+                <h2 className="header foregreen"> Senior Photos </h2>
                 <div className="container">
                     <Link to="/seniorphotos/2017">
-                        <div className="control">
+                        <div className="control foregreen">
                             2017
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2016">
-                        <div className="control">
+                        <div className="control foregreen">
                             2016
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2015">
-                        <div className="control">
+                        <div className="control foregreen">
                             2015
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2014">
-                        <div className="control">
+                        <div className="control foregreen">
                             2014
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2013">
-                        <div className="control">
+                        <div className="control foregreen">
                             2013
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2012">
-                        <div className="control">
+                        <div className="control foregreen">
                             2012
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2011">
-                        <div className="control">
+                        <div className="control foregreen">
                             2011
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2010">
-                        <div className="control">
+                        <div className="control foregreen">
                             2010
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2009">
-                        <div className="control">
+                        <div className="control foregreen">
                             2009
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2008">
-                        <div className="control">
+                        <div className="control foregreen">
                             2008
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2007">
-                        <div className="control">
+                        <div className="control foregreen">
                             2007
                         </div>
                     </Link>
                     <Link to="/seniorphotos/2006">
-                        <div className="control">
+                        <div className="control foregreen">
                             2006
                         </div>
                     </Link>
