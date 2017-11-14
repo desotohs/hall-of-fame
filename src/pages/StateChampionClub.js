b4ab78ab7b3512c4a3e0c4173e96bb05dba48374
Zach Deibert <zachdeibert@gmail.com>
2017-11-13 21:02:22 -0600

    Fixed unused variables

--- src/pages/StateChampionClub.js
+++ src/pages/StateChampionClub.js
@@ -18,7 +18,7 @@ import tennis from "../media/images/Tennis.png";
 import theatre from "../media/images/Theatre.png";
 import volleyball from "../media/images/Volleyball.png";
 */
-import image from "../database/failover/image-error.png";
+//import image from "../database/failover/image-error.png";
 import "./StateChampionClub.css";
 
 export default class StateChampions extends React.Component {
@@ -56,6 +56,7 @@ function doTitle(club){
     return result
 }
 
+/*
 function doImageName(club){
     var words = club.split("-")
     var result = "";
@@ -64,6 +65,7 @@ function doImageName(club){
     }
     return result;
 }
+*/
 
 function doCamelCaseIcon(club){
     var words = club.split("-");
