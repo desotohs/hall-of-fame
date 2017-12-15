d575c374e11f0388ecf6963b28a5594bf5c51d6b
Zach Deibert <zachdeibert@gmail.com>
2017-12-15 10:19:16 -0600

    Launcher script update

--- .launch.sh
+++ .launch.sh
@@ -18,3 +18,7 @@ firefox "http://kpirankings.wixsite.com:3000/dhswildcats" &
     echo "exit"
 ) | java -cp AWTRobotShell/bin deibert.zach.awtrobotshell.Main
 xinput set-prop 10 "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
+for win in $(xdotool search --onlyvisible --name firefox); do
+    xdotool windowsize $win 1080 1920
+    xdotool windowmove $win 0 0
+done
