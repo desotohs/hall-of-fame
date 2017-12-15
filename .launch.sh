e258f1da32300cc381858bca47a70c3218120c94
Zach Deibert <zachdeibert@gmail.com>
2017-12-15 10:08:07 -0600

    Launcher script update

--- .launch.sh
+++ .launch.sh
@@ -4,7 +4,16 @@ cd "$(dirname "$0")"
 
 sudo X &
 export DISPLAY=:0
+xrandr -o left
+xinput set-prop 10 "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 nvm use --delete-prefix node
 node proxy.js &
 sleep 2s
 firefox "http://kpirankings.wixsite.com:3000/dhswildcats"
+(
+    sleep 2s
+    echo "press key 122"
+    sleep 0.2s
+    echo "release key 122"
+    echo "exit"
+) | java -cp bin deibert.zach.awtrobotshell.Main
