9c58e50cdc039c0dd4c3fb36856bdcff2d4091bc
Zach Deibert <zachdeibert@gmail.com>
2017-12-15 09:49:44 -0600

    Fixed launcher script

--- .launch.sh
+++ .launch.sh
@@ -5,4 +5,5 @@ cd "$(dirname "$0")"
 sudo X &
 export DISPLAY=:0
 nvm use --delete-prefix node
-node_modules/.bin/electron .
+node proxy.js
+firefox "http://kpirankings.wixsite.com:3000/dhswildcats"
