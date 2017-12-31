367a071742de9832d9dd466f928c93c1ba2fde56
Zach Deibert <zachdeibert@gmail.com>
2017-12-31 17:09:52 -0600

    Created project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.DesotoHS.HallOfFame {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
