9b26ad33b3a3c368aa1c8a136a1346497e3f57cd
Zach Deibert <zachdeibert@gmail.com>
2018-01-03 08:01:13 -0600

    Removed graphics testing harness

--- Ui/GraphicsLoader.cs
+++ Ui/GraphicsLoader.cs
@@ -1,27 +1,17 @@
 using System;
 using System.Drawing;
 using Com.Latipium.Core;
-using Com.GitHub.DesotoHS.HallOfFame.Ui.Components;
+using Com.GitHub.DesotoHS.HallOfFame.Ui.Components.Pages;
 
 namespace Com.GitHub.DesotoHS.HallOfFame.Ui {
     [LatipiumLoader]
     public static class GraphicsLoader {
-        static void CreateRect(Window window, Color color, byte alpha, float x, float y, float width, float height) {
-            Component test = new Component();
-            test.Background = Color.FromArgb(alpha, color);
-            test.Bounds = new RectangleF(x, y, width, height);
-            window.Add(test);
-        }
-
         [LatipiumLoadMethod]
         public static void Start() {
             Window win = new Window();
-            CreateRect(win, Color.Red, 255, 0, 0, 0.5f, 0.5f);
-            CreateRect(win, Color.Green, 255, 0, 0.6f, 0.8f, 0.3f);
-            CreateRect(win, Color.Blue, 127, 0.2f, 0.1f, 0.5f, 0.7f);
-            Test test = new Test();
-            test.Bounds = new RectangleF(0.3f, 0.2f, 0.6f, 0.5f);
-            win.Add(test);
+            MainMenu menu = new MainMenu();
+            menu.Bounds = new RectangleF(0, 0, 1, 1);
+            win.Add(menu);
         }
     }
 }
